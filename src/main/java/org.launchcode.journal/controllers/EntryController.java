f741690c6ec7bc058d200a573810e4b1142a79d8
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-02-23 22:06:06 -0500

    second commit

--- src/main/java/org.launchcode.journal/controllers/EntryController.java
+++ src/main/java/org.launchcode.journal/controllers/EntryController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -32,9 +33,12 @@ public class EntryController {
     public String addEntry(Model model){
         model.addAttribute("entry", new Entry());
         model.addAttribute("pageTitle","Add entry");
-        model.addAttribute("name","Add entry");
+        model.addAttribute("entries", entryRepository.findAll());
+        model.addAttribute("topics", topicRepository.findAll());
+        model.addAttribute("moods", moodRepository.findAll());
         return "entries/add";
     }
+
 
     @GetMapping("list")
     public String listEntry(Model model){
@@ -44,10 +48,30 @@ public class EntryController {
     }
 
     @PostMapping("add")
-    public String saveEntry(Entry entry){
-        System.out.println(entry);
-        entryService.save(entry);
-        return "redirect:/entries/list";
+    public String processAddEntryForm(@ModelAttribute @Valid Entry newEntry, Model model,
+                                      Errors errors, @RequestParam int topic_id) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute(new Entry());
+            model.addAttribute("entries", entryRepository.findAll());
+            model.addAttribute("topics", topicRepository.findAll());
+            model.addAttribute("moods", moodRepository.findAll());
+
+
+            return "add";
+        }
+        Optional<Topic> topic = topicRepository.findById(topic_id);
+        if (topic.isPresent()) {
+            newEntry.setTopic(topic.get());
+        }
+
+        Optional<Mood> mood = moodRepository.findById(topic_id);
+        if (mood.isPresent()) {
+            newEntry.setMood(mood.get());
+        }
+
+        entryRepository.save(newEntry);
+        return "redirect:";
     }
 
     @GetMapping("/edit/{id}")
@@ -73,6 +97,7 @@ public class EntryController {
         Entry existingEntry = entryRepository.findById(id)
                 .orElseThrow(() -> new UserNotFoundException("Entry not found with id: " + id));
         existingEntry.setTitle(entry.getTitle());
+        existingEntry.setJournalEntry(entry.getJournalEntry());
         entryRepository.saveAndFlush(existingEntry);
         ra.addFlashAttribute("message", "Entry updated successfully");
         return "redirect:/entries/list";
